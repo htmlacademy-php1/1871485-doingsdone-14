ddf12e7a565a323b8f3e1711ec6776374bf6c088
Олеся <88152339+BlackLesenok@users.noreply.github.com>
2022-04-03 16:17:29 +0300

    Update index.php

--- index.php
+++ index.php
@@ -71,7 +71,7 @@ $show_complete_tasks = rand(0, 1);
                     </nav>
 
                     <label class="checkbox">
-                        <input type="checkbox" checked>
+                        <input type="checkbox" <?php checked( $show_complete_tasks['1'] ); ?> >
                         <input class="checkbox__input visually-hidden show_completed" type="checkbox">
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
